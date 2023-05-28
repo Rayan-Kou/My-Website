703bbc1dd359b931fb2c8fffbd4e77a2c1efacd8
Rayan Koutchoukali <rnk3hz@umsystem.edu>
2023-05-28 06:09:58 -0500

    Added support for arranging elements horizontally *and* vertically (fix)

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="portrait.jpg" alt="Portrait" className="small-image rounded-image" />
+        <div className="horizontal">
+          <img src="portrait.jpg" alt="Portrait" className="small-image rounded-image" />
 
-        <p>
-          <b>Hi, I'm Rayan Koutchoukali.</b><br />
-          This is my website that I created with React. If you want to look through the code, here is the <a href="https://github.com/TheChunkyPotato/My-Website">GitHub repo</a>.
-        </p>
+          <p>
+            <b>Hi, I'm Rayan Koutchoukali.</b><br />
+            This is my website that I created from scratch with React. If you want to look through the code, here is the <a href="https://github.com/TheChunkyPotato/My-Website">GitHub repo</a>.
+          </p>
+        </div>
 
-        <p>
-          <a
-            className="App-link"
-            href="https://www.linkedin.com/in/rayan-koutchoukali/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-        </p>
+        <div className="vertical">
+          <p>
+            <a
+              className="App-link"
+              href="https://www.linkedin.com/in/rayan-koutchoukali/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </p>
+        </div>
       </header>
     </div>
   );
