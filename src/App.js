3c3f51861570c99fdc67dd7cdadbce305c3779df
Rayan Koutchoukali <rnk3hz@umsystem.edu>
2023-05-28 05:25:43 -0500

    Initial functionality with name, portrait, and LinkedIn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="headline-text">
+          Rayan Koutchoukali
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <img src="portrait.jpg" alt="Portrait" className="small-image rounded-image" />
+
+        <p>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/rayan-koutchoukali/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
       </header>
     </div>
   );
