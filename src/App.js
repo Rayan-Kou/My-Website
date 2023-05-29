cc4c61ab759b1801d5f2e85df6fc676b6f917dc8
Rayan Koutchoukali <rnk3hz@umsystem.edu>
2023-05-29 04:56:59 -0500

    Reworked entire site structure

--- src/App.js
+++ src/App.js
@@ -1,55 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="horizontal">
-          <img src="portrait.jpg" alt="Portrait" className="small-image rounded-image" />
-
-          <p>
-            <b>Hi, I'm Rayan Koutchoukali.</b><br />
-            This is my website that I created from scratch with React. If you want to look through the code, here is the <a href="https://github.com/TheChunkyPotato/My-Website">GitHub repo</a>.
-          </p>
+    <div>
+      <header className="menu-bar">
+        <div className="menu-item">
+            Home
         </div>
-
-        <div className="vertical">
-          <p className="large-text">
-            I'm seeking an internship or a job in roles related to software programming and IT.
-          </p>
-
-          <div className="horizontal">
-          <p>
-            <div className="centered-text">
-              <b>Education</b><br />
-            </div>
-            <ul>
-              <li>Senior at Missouri S&T</li>
-              <li>Majoring in Computer Science</li>
-              <li>Graduating in December 2023</li>
-            </ul>
-          </p>
-
-          <p>
-            <div className="centered-text">
-              <b>Experience</b><br />
-            </div>
-            <ul>
-              <li>C++</li>
-              <li>Python</li>
-              <li>Java</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>PHP</li>
-              <li>SQL</li>
-              <li>C#</li>
-              <li>Rust</li>
-            </ul>
-          </p>
-          </div>
+        <div className="menu-item">
+            Contact
         </div>
       </header>
+      <div className="content">
+        <div className={`content-section ${fadeIn ? 'fade-in' : ''}`}>
+          <div className="content-item">
+            <img src="portrait.jpg" alt="Portrait" className="portrait" />
+          </div>
+          <div className="content-item">
+            <b>Hi, I'm Rayan Koutchoukali.</b><br />
+            This is my website that I created from scratch with React. If you want to look through the code, here is the <a href="https://github.com/TheChunkyPotato/My-Website">GitHub repo</a>.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/old-App.js
@@ -0,0 +1,57 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="horizontal">
+          <img src="portrait.jpg" alt="Portrait" className="small-image rounded-image" />
+
+          <p>
+            <b>Hi, I'm Rayan Koutchoukali.</b><br />
+            This is my website that I created from scratch with React. If you want to look through the code, here is the <a href="https://github.com/TheChunkyPotato/My-Website">GitHub repo</a>.
+          </p>
+        </div>
+
+        <div className="vertical">
+          <p className="large-text">
+            I'm seeking an internship or a job in roles related to software programming and IT.
+          </p>
+
+          <div className="horizontal">
+          <p>
+            <div className="centered-text">
+              <b>Education</b><br />
+            </div>
+            <ul>
+              <li>Senior at Missouri S&T</li>
+              <li>Majoring in Computer Science</li>
+              <li>Graduating in December 2023</li>
+            </ul>
+          </p>
+
+          <p>
+            <div className="centered-text">
+              <b>Experience</b><br />
+            </div>
+            <ul>
+              <li>C++</li>
+              <li>Python</li>
+              <li>Java</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>PHP</li>
+              <li>SQL</li>
+              <li>C#</li>
+              <li>Rust</li>
+            </ul>
+          </p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
